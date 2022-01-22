74be814d3320253e3b6c7bef434052a3059355f1
tami5 <kkharji@protonmail.com>
2022-01-22 20:31:00 +0300

    feat(user): add top-level module for handling user

---
+++ src/user.nix
@@ -0,0 +1,56 @@
+{ mkUserHome }:
+let
+  injectHome = username: userConfig: {
+    home-manager.users."${username}" = (mkUserHome userConfig);
+  };
+
+in {
+
+  # Common, Applied with nixos and darwin.
+  common = { lib, pkgs, config, user, ... }:
+    let username = if lib.hasAttr "username" user then user.username else null;
+    in {
+      options.nixenv = {
+        username = lib.mkOption {
+          type = lib.types.str;
+          default = username;
+          description = "user's name";
+        };
+
+        shell = lib.mkOption {
+          type = lib.types.package;
+          default = pkgs.zsh;
+          description = "user's shell";
+        };
+
+        # TODO: make sure it's either a path or a derivation
+        home = lib.mkOption {
+          # type =  lib.types.either lib.types.path (lib.types.functionTo lib.types.attrs);
+          default = null;
+          description = "function to initialize user in home-manager aspect.";
+        };
+      };
+    };
+
+  # Only Applied darwin system.
+  darwin = { lib, pkgs, config, ... }: {
+    config = lib.mkMerge [({
+      # Enable zsh in order to add /run/current-system/sw/bin to $PATH
+      # TODO: should this be default??
+      programs.zsh.enable = true;
+
+      # Setup user user
+      users.users = with config.nixenv; {
+        "${username}" = {
+          description = "...";
+          shell = shell;
+          home = "/Users/${username}";
+        };
+      };
+    })
+    (lib.mkIf (!isNull config.nixenv.home)
+      (injectHome config.nixenv.username config.nixenv.home))
+    # (tryToInjectHome config.nixenv.username config.nixenv.home)
+      ];
+  };
+}
